fce50642b1ae8404855376b44e7cb14fa0fcc95e
Timothy Krause <tmkrs4482@gmail.com>
2024-12-24 15:51:32 -0700

    Starting the user interface.

---
+++ VocoderSynthUI.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string>
+#include <lv2/lv2plug.in/ns/lv2core/lv2.h>
+#include <lv2/lv2plug.in/ns/extensions/ui/ui.h>
+#include "xputty.h"
+#include "xwidget.h"
+
+class VocoderSynthUI
+{
+private:
+    LV2UI_Write_Function write_function;
+    LV2UI_Controller controller;
+    Widget_t* dial;
+    Widget_t* box;
+    Xputty main;
+
+public:
+    VocoderSynthUI (LV2UI_Write_Function write_function, LV2UI_Controller controller, void* parentXWindow, std::string bundlePath);
+    ~VocoderSynthUI();
+    LV2UI_Widget getTopLevelWidget();
+    Xputty* getXputty();
+    void portEvent(uint32_t port_index, uint32_t buffer_size, uint32_t format, const void *buffer);
+    static void valueChangedCallback (void* obj, void* data);
+    static void exposeCallback (void* obj, void* data);
+};
